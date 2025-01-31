56eab15edd908f74d64873c7fe01c6521604a932
andrew-data-git <ajrowley3000@gmail.com>
2025-01-31 09:02:48 +0000

    staging done

--- build_db.py
+++ build_db.py
@@ -28,7 +28,6 @@ conn.autocommit = True
 
 ### Build staging table
 
-# cheeses
 cursor.execute('''
                CREATE TABLE IF NOT EXISTS cheeses(
                cheese_id INT,
@@ -56,10 +55,75 @@ cursor.execute('''
 
 ### Build all other tables
 
-# fct_cheese
+# dim_rind
 cursor.execute('''
-               CREATE TABLE IF NOT EXISTS fct_cheese(
-               cheese_id INT PRIMARY KEY,
+               CREATE TABLE IF NOT EXISTS dim_rind(
+               rind_id SERIAL PRIMARY KEY,
+               rind TEXT
+               );
+               ''') 
+
+# dim_animal
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS dim_animal(
+               animal_id SERIAL PRIMARY KEY,
+               animal TEXT
+               );
+               ''') 
+
+# dim_country
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS dim_country(
+               country_id SERIAL PRIMARY KEY,
+               country TEXT
+               );
+               ''') 
+
+# dim_producers
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS dim_producers(
+               producers_id SERIAL PRIMARY KEY,
+               producers TEXT
+               );
+               ''') 
+
+# dim_type
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS dim_type(
+               type_id SERIAL PRIMARY KEY,
+               type TEXT
+               );
+               ''') 
+
+# dim_texture
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS dim_texture(
+               texture_id SERIAL PRIMARY KEY,
+               texture TEXT
+               );
+               ''') 
+
+# dim_colour
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS dim_colour(
+               colour_id SERIAL PRIMARY KEY,
+               colour TEXT,
+               simple_colour VARCHAR(15)
+               );
+               ''') 
+
+# dim_taste
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS dim_taste(
+               taste_id SERIAL PRIMARY KEY,
+               taste TEXT
+               );
+               ''') 
+
+# dim_cheese
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS dim_cheese(
+               cheese_id SERIAL PRIMARY KEY,
                animal_id INT,
                country_id INT,
                cheese TEXT,
@@ -70,105 +134,65 @@ cursor.execute('''
                );
                ''') 
 
-# dim_rind
-cursor.execute('''
-               CREATE TABLE IF NOT EXISTS dim_rind(
-               rind_id INT PRIMARY KEY,
-               rind TEXT
-               );
-               ''') 
+### Populate cheeses
 
-# dim_animal
-cursor.execute('''
-               CREATE TABLE IF NOT EXISTS dim_animal(
-               animal_id INT PRIMARY KEY,
-               animal TEXT
-               );
-               ''') 
-
-# dim_country
-cursor.execute('''
-               CREATE TABLE IF NOT EXISTS dim_country(
-               country_id INT PRIMARY KEY,
-               country TEXT
-               );
-               ''') 
-
-# dim_producers
-cursor.execute('''
-               CREATE TABLE IF NOT EXISTS dim_producers(
-               producers_id INT PRIMARY KEY,
-               producers TEXT
-               );
-               ''') 
-
-# dim_type
-cursor.execute('''
-               CREATE TABLE IF NOT EXISTS dim_type(
-               type_id INT PRIMARY KEY,
-               type TEXT
-               );
-               ''') 
-
-# dim_texture
-cursor.execute('''
-               CREATE TABLE IF NOT EXISTS dim_texture(
-               texture_id INT PRIMARY KEY,
-               texture TEXT
-               );
-               ''') 
-
-# dim_colour
-cursor.execute('''
-               CREATE TABLE IF NOT EXISTS dim_colour(
-               colour_id INT PRIMARY KEY,
-               colour TEXT,
-               simple_colour VARCHAR(15)
-               );
-               ''') 
-
-# dim_taste
-cursor.execute('''
-               CREATE TABLE IF NOT EXISTS dim_taste(
-               taste_id INT PRIMARY KEY,
-               taste TEXT
-               );
-               ''') 
-
-### Populate tables
-
-# cursor.execute('''
-#                COPY cheeses(
-#                cheese_id,
-#                cheese,
-#                url,
-#                milk,
-#                country,
-#                region,
-#                family,
-#                type,
-#                fat_content,
-#                calcium_content,
-#                texture,
-#                rind,
-#                color,
-#                flavor,
-#                aroma,
-#                vegetarian,
-#                vegan,
-#                synonyms,
-#                alt_spellings,
-#                producers
-#                ) 
-#                 FROM '/home/andrew/programming/datasets/cheese/cheeses_processed.csv' 
-#                 DELIMITER '\t' 
-#                 CSV HEADER;
-#                ''')
-
+cursor.execute('''TRUNCATE TABLE cheeses RESTART IDENTITY''')
 with open('/home/andrew/programming/datasets/cheese/cheeses_processed.csv') as csv_file:
     next(csv_file) # skip headers
     cursor.copy_from(csv_file, 'cheeses', sep='\t')
     conn.commit()
+
+### ### ### ### ### ### ###
+
+# 1. Extract unique values from the cheeses table to populate your dim_cheese and dim_animal dimension tables.
+# 2. Insert the unique values into the dimension tables.
+# 3. Populate the fact table or update the original cheeses table to use the new dimension table keys.
+
+# 1.1
+# INSERT INTO dim_cheese (cheese_name, cheese_type, cheese_flavor_profile)
+# SELECT DISTINCT cheese_name, cheese_type, cheese_flavor_profile
+# FROM cheeses
+# ON CONFLICT (cheese_name) DO NOTHING;
+
+# 1.2
+# INSERT INTO dim_animal (animal_type)
+# SELECT DISTINCT animal_type
+# FROM cheeses
+# WHERE animal_type IS NOT NULL
+# ON CONFLICT (animal_type) DO NOTHING;
+
+
+# Step 2: Populate the Fact Table with Foreign Keys
+
+# Once the dimension tables are populated, you'll need to link them back to the cheeses table (or insert them into a fact 
+# table, depending on your use case). To do this, we will update the cheeses table (or use a fact table) to reference the 
+# keys from dim_cheese and dim_animal.
+
+# Assuming you want to update the cheeses table with the cheese_id and animal_id from the dimension tables, you can 
+# run the following updates.
+# 2.1 Update cheeses Table with Foreign Keys
+
+# -- Update the cheeses table with the cheese_id from dim_cheese
+# UPDATE cheeses
+# SET cheese_id = dc.cheese_id
+# FROM dim_cheese dc
+# WHERE cheeses.cheese_name = dc.cheese_name;
+
+# -- Update the cheeses table with the animal_id from dim_animal
+# UPDATE cheeses
+# SET animal_id = da.animal_id
+# FROM dim_animal da
+# WHERE cheeses.animal_type = da.animal_type;
+
+# These UPDATE queries will:
+
+#     Link the cheeses table to the dim_cheese and dim_animal dimension tables.
+#     Use the cheese_name and animal_type columns to match the corresponding records in the dimension 
+# tables and update the cheese_id and animal_id foreign keys in the cheeses table.
+
+
+
+
 
 ### ### ### ### ### ### ###
 
